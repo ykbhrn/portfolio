d987de30ca4d355ffeb46485e6e092b763beb875
ykbhrn <jakub.horun@gmail.com>
2020-07-01 15:12:53 +0100

    homepage done

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,18 @@ function init() {
 
   const cursor = document.getElementById('cursor')
   const projects = document.getElementById('project-section')
+  const projectDetailView = document.querySelector('.projects-detail-view')
+  const slider = document.querySelector('.projects')
   const about = document.getElementById('about')
   const experience = document.getElementById('experience')
   const sectionOne = document.getElementById('home')
   const header = document.querySelector('.menu')
+  const projectIconOne = document.querySelector('.project-icon-one')
+  const projectIconAll = document.querySelector('.project-icon-two')
+  const projectFirst = document.querySelector('.project-first')
+  const projectSecond = document.querySelector('.project-second')
+  const next = document.querySelector('.next')
+  const previous = document.querySelector('.previous')
 
   function scroll() {
     const selected = document.querySelector('.selected-nav')
@@ -69,15 +77,8 @@ function init() {
     
   }
 
-  function cursorHover() {
-    cursor.style.background = 'red'
-  }
 
-  function cursorHoverOut() {
-    cursor.style.background = 'black'
-  }
-
-  function activeNavbar(event){
+  function activeNavbar(event){ 
     const selected = document.querySelector('.selected-nav')
     if (selected !== null) {
       selected.classList.remove('selected-nav')
@@ -85,16 +86,39 @@ function init() {
     event.target.className = 'selected-nav'
   }
 
+  function showAll(){
+    slider.style.display = 'none'
+    projectDetailView.style.display = 'flex'
+  }
+
+  function showOne(){
+    projectDetailView.style.display = 'none'
+    slider.style.display = 'block'
+  }
+
+  function showFirst() {
+    projectFirst.style.display = 'flex'
+    projectSecond.style.display = 'none'
+  }
+
+  function showSecond() {
+    projectSecond.style.display = 'flex'
+    projectFirst.style.display = 'none'
+  }
+
+
   document.addEventListener( 'mousemove', cursorMove)
-  projects.addEventListener( 'mouseenter', cursorHover)
-  projects.addEventListener( 'mouseleave', cursorHoverOut)
   header.addEventListener('click', activeNavbar)
   projects.addEventListener('mouseenter', scroll)
   projects.addEventListener('mouseleave', scrollOut)
   about.addEventListener('mouseenter', scrollAbout)
   about.addEventListener('mouseleave', scrollOutAbout)
-  experience.addEventListener('mouseenter', scrollExperience)
-  experience.addEventListener('mouseleave', scrollOutExperience)
+  // experience.addEventListener('mouseenter', scrollExperience)
+  // experience.addEventListener('mouseleave', scrollOutExperience)
+  projectIconOne.addEventListener('click', showOne)
+  projectIconAll.addEventListener('click', showAll)
+  previous.addEventListener('click', showFirst)
+  next.addEventListener('click', showSecond)
 }
 
 window.addEventListener('DOMContentLoaded', init)
